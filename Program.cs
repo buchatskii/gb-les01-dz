b2b2b33202e195ace0e72378d5f01efeb5460595
Artur <buch16@bk.ru>
2022-08-08 01:58:39 +0300

    Решил 2,4,6 и 7 задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 > number2)
+// {
+//     Console.WriteLine(number1);
+// }
+// else
+// {
+//     Console.WriteLine(number2);
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.Write("");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 > number2 & number1 > number3)
+// {
+//     Console.WriteLine(number1);
+// }
+// if (number2 > number1 & number2 > number3)
+// {
+//     Console.WriteLine(number2);
+// }
+// else
+// {
+//     Console.WriteLine(number3);
+// }
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number % 2 == 0)
+// {
+//    Console.WriteLine("Четное");
+// }
+// else
+// {
+//     Console.WriteLine("Ошибка");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача 7: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// number = number % 10;
+
+// Console.WriteLine(number);
